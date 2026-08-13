66cb8942224ce3971cdc99bd46853464874143f0
Kevin Gomez <147205806+KevinMec@users.noreply.github.com>
2026-08-13 05:13:09 -0700

    Add files via upload

---
+++ SESSION-07/old Batch_Notes.sql
@@ -0,0 +1,97 @@
+THE DATA CAN BE IN TWO FORMATS
+
+TABLE
+FILE
+
+
+FILES
+----
+
+THEREE TYPES :
+
+
+STRUCTURED FILES        : DATA FILE / FLAT FILE (90%)
+SEMI STRUCTURED FILES   : XML FILES (10%) , HTML
+UN STRUCTURED FILES     : PDF , IMAGE , AUDIO , VIDEO , EXCEL , WORD , PPT ,ZIP
+
+
+DATA FILE / FLAT FILE :
+
+FLAT FILES ARE TWO TYPES
+
+1.FIXED WIDTH FLAT  FILES  ( EACH COLUMN IS SEPERATED SINGLE SPACE , DOUBLE SPACE)
+2.DELIMITED FLAT FILES    ( EACH COLUMN IS SEPRATED BY DELIMETER)
+
+column seperator  - DELIMETOR
+
+
+FILE TYPES 
+
+.txt 
+.dat 
+.XML
+.pdf
+.jpeg
+.EXE
+.doc
+.xls
+.ppt
+.csv
+.OUT
+.zip
+.png
+
+
+
+filename.extension
+
+List.txt 
+
+in reatilme we will always Flat files/ data files.(DELIMITED FLAT FILES)
+
+Most common FLAT file types in realtime.
+
+.DAT  - data file  						 Eg. Customers.dat
+.txt  - text file  						 Eg. List.txt
+.OUT  - output file 					 Eg. cust_tgt.out
+.csv  - comma seperated value  file.     Eg: orders.csv
+
+
+each datafle or  flat file contains a single delimiter
+
+most common delimters we will use real time.
+
+, 
+|    pipe
+~    tild
+^    cap , carrot 
+;   
+tab  4   spaces
+
+
+.DAT  - data file    ( , | ~ ^ ;  tab)
+.txt  - text file  	 ( , | ~ ^ ;  tab)				 
+.OUT  - output file  ( , | ~ ^ ;  tab)					 
+.csv  - comma seperated value  file.  ( the delimiter should comma only )
+
+
+
+informatica downloaded folder 
+
+informatica installed folder
+
+
+
+
+CRAEATION OF TARGET TABLE :
+
+1. CREATE A TARGET TABLE MNAUALLY BY WRITING CREATE TABLE IN TARGET SCHEMA IN ORACLE DBA 
+   AND IMPORT THIS TARGET IN INFORMATICA TARGET DESIGNER
+
+2. DIRECTLY CREATE A TABLE IN INFORMATICA , THEN EXECUTE THIS TABLE TO ORACLE FROM INFORMATICA 
+
+
+Source file directory = $PMSourceFileDir\
+
+
+$PMSourceFileDir\ = D:\Informatica\server\infa_shared\SrcFiles
